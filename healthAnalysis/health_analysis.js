57d75b4d211972b7f60700c3b019231e15687d26
christianc-coder <crist200555@gmail.com>
2026-05-04 16:04:20 -0400

    ok

--- healthAnalysis/health_analysis.js
+++ healthAnalysis/health_analysis.js
@@ -1,17 +1,30 @@
 const addPacienteButton = document.getElementById('addPatient');
 const reporte = document.getElementById('report');
 const btnSearch = document.getElementById('btnSearch');
-const patients = [];
+const pacients = [];
 
 function addPacient() {
  const name = document.getElementById('name').value;
  const gender = document.querySelector('input[name="gender"]:checked');
  const age = document.getElementById('age').value;
  const condition = document.getElementById('condition').value;
-
+ 
+ // Empuja a el paciente a un arreglo para su manipulacion
  if(name && gender && age && condition){
-    patients.push({ name, genero: gender.value, age, condition});
+    pacients.push({ name, genero: gender.value, age, condition});
     resetForm();
     generateReport();
  }
+}
+function resetForm (){
+    const name = document.getElementById('name').value = "";
+    const gender = document.querySelector('input[name="gender"]:checked').checked = false;
+    const age = document.getElementById('age').value = "";
+    const condition = document.getElementById('condition').value = "";
+    
+}
+function generateReport (){
+    const numPacient = pacients.length;
+    
+
 }
